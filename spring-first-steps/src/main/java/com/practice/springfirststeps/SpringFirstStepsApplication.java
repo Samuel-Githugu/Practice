72b2bd9ce6215d9828a373e78bcecd61f0554019
Samuel-Githugu <githugu.samuel@gmail.com>
2025-05-27 16:08:47 +0200

    added HTML, removed hello world

--- spring-first-steps/src/main/java/com/practice/springfirststeps/SpringFirstStepsApplication.java
+++ spring-first-steps/src/main/java/com/practice/springfirststeps/SpringFirstStepsApplication.java
@@ -13,9 +13,10 @@ public class SpringFirstStepsApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringFirstStepsApplication.class, args);
     }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
-    }
+//    Hello world initial implementation
+//    @GetMapping("/hello")
+//    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+//        return String.format("Hello %s!", name);
+//    }
 
 }
